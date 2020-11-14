9685ff778f29de7010fb9d6edf9ecb33fa4e4f17
tediko <tediko123@wp.pl>
2020-11-14 11:32:31 +0100

    Added event.preventDefault() to validateEmail

--- js/app.js
+++ js/app.js
@@ -3,10 +3,11 @@ const submitBtn = document.querySelector('[data-btn]');
 const input = document.querySelector('[data-input]');
 
 // Validate input and add invalid class to form if input is invalid
-const validateEmail = () => {
+const validateEmail = (event) => {
     const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
     if (!emailRegex.test(input.value) || input.value == '') {
         form.classList.add('invalid');
+        event.preventDefault();
     } else {
         form.submit();
     }
